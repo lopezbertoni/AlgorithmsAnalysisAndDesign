b5b0e3d264bfe95641342a65db938b87fd34cb22
lopezbertoni <lopezbertoni@gmail.com>
2015-02-19 17:04:46 -0300

    Read Data Routine and TestData

--- ProgrammingQuestion3/Program.cs
+++ ProgrammingQuestion3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,33 @@ namespace ProgrammingQuestion3
     {
         static void Main(string[] args)
         {
+            var testCase1 = ReadData("TestCase1.txt");
+        }
+
+        private static Dictionary<int, List<int>> ReadData(string filename)
+        {
+            var txtData = File.ReadLines(filename).ToArray();
+            var inputData = new Dictionary<int, List<int>>();
+
+            foreach (var s in txtData)
+            {
+                var x = s.Split('\t');
+                var v = 0;
+                var e = new List<int>();
+                for (var i = 0; i < x.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        v = Convert.ToInt32(x[i]);
+                    }
+                    else
+                    {
+                        e.Add(Convert.ToInt32(x[i]));
+                    }
+                }
+                inputData.Add(v, e);
+            }
+            return inputData;
         }
     }
 }
