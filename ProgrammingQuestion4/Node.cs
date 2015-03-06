92b4ab316ae359d717aa411911d82556c9e3ead9
Federico Javier Lopez Bertoni <federico@greenlight.com.py>
2015-03-06 19:09:46 -0300

    Test cases
    initial read data

---
+++ ProgrammingQuestion4/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingQuestion4
+{
+    public class Node
+    {
+        public int V { get; set; }
+        public List<int> Edges { get; set; }
+        public bool Visited { get; set; }
+    }
+}
--- ProgrammingQuestion4/Program.cs
+++ ProgrammingQuestion4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,44 @@ namespace ProgrammingQuestion4
     {
         static void Main(string[] args)
         {
+            var test = new Stack<Int32>();
+            var t1 = ReadData("TestCase1.txt");
+            var t2 = ReadData("TestCase1.txt");
+            var t = ReadData("SCC.txt"); 
         }
+
+        private static List<Node> ReadData(string filename)
+        {
+            var txtData = File.ReadLines(filename).ToArray();
+            var inputData = new List<Node>();
+
+            var v = 0;
+            var e = new List<int>();
+            foreach (var s in txtData)
+            {
+                var x = s.Split(' ');
+                
+                if (Convert.ToInt32(x[0]) != v)
+                {
+                    for (var i = 0; i < x.Length; i++)
+                    {
+                        if (i == 0)
+                        {
+                            v = Convert.ToInt32(x[i]);
+                        }
+                        else
+                        {
+                            if (!String.IsNullOrEmpty(x[i]))
+                            {
+                                e.Add(Convert.ToInt32(x[i]));
+                            }
+                        }
+                    }
+                }
+                inputData.Add(new Node {V = v, Edges = e, Visited = false});
+            }
+            return inputData;
+        }
+
     }
 }
