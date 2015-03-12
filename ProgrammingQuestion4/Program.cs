2da3b984922196a67de8b847491f1d66c33f769d
Federico Javier Lopez Bertoni <federico@greenlight.com.py>
2015-03-12 18:35:17 -0300

    More changes

--- ProgrammingQuestion4/Program.cs
+++ ProgrammingQuestion4/Program.cs
@@ -12,17 +12,21 @@ namespace ProgrammingQuestion4
     {
         static void Main(string[] args)
         {
+
+            ComputeScc("TestCase1.txt");
             //var testCase1 = ReadData("TestCase1.txt");
             //DfsLoop(testCase1);
-            t = 0;
-            var testCase2 = ReadData("TestCase2.txt");
-            DfsLoop(testCase2);
+            //t = 0;
+            //_finishingTimes.Clear();
+            //var testCase2 = ReadData("TestCase2.txt");
+            //DfsLoop(testCase2);
+
             //t = 0;
             //var testCase3 = ReadData("TestCase3.txt");
             //DfsLoop(testCase3);
-            t = 0;
-            var testCase4 = ReadData("TestCase4.txt");
-            DfsLoop(testCase4);
+            //t = 0;
+            //var testCase4 = ReadData("TestCase4.txt");
+            //DfsLoop(testCase4);
             //var testCase5 = ReadData("TestCase5.txt");
             //DfsLoop(testCase5);
             //var testCase6 = ReadData("TestCase6.txt");
@@ -45,13 +49,21 @@ namespace ProgrammingQuestion4
             //DfsLoop(t);
         }
 
-        private static List<String> ComputeScc(List<Node> graph)
+        private static int t = 0;
+        private static readonly Stack<int> FinishingTimes = new Stack<int>(); 
+        private static readonly Dictionary<int, int> Results = new Dictionary<int, int>(); 
+        private static List<String> ComputeScc(string filename)
         {
             var results = new List<String>();
+            t = 0;
+            FinishingTimes.Clear();
 
             //Compute DFS on reverse graph.
-            var grev = graph.Select(n => new Node { V = n.E, E = n.V }).ToList();
-            DfsLoop(graph);
+            var grev = ReadDataReversed(filename);
+            DfsLoop(grev);
+            //Compute DFS on graph
+            var graph = ReadData(filename);
+            DfsLoopStack(graph);
             return results;
         }
 
@@ -68,6 +80,25 @@ namespace ProgrammingQuestion4
                     DfsRecursive(graph, index);
                 }
                 index--;
+            }
+            foreach (var x in FinishingTimes)
+            {
+                Console.WriteLine("Node index {0} with value {1}", x, graph[x].V);
+            }
+        }
+
+        private static void DfsLoopStack(List<Node> graph)
+        {
+            while (FinishingTimes.Count > 0)
+            {
+                var index = FinishingTimes.Pop();
+                var node = graph[index];
+                if (!node.Visited)
+                {
+                    Results.Add(graph[index].V, 0);
+                    Console.WriteLine("Checking index {0} with value {1}", index, graph[index].V);
+                    DfsRecursive(graph, index);
+                }
             }
         }
 
@@ -111,12 +142,11 @@ namespace ProgrammingQuestion4
             Console.WriteLine("Finishing Time = {0}", f);
         }
 
-        private static int t = 0;
         private static void DfsRecursive(List<Node> graph, int nodeIndex)
         {
             //Mark nodes as visited
             graph[nodeIndex].Visited = true;
-            
+            var sccCount = 1;
             foreach (var edge in graph[nodeIndex].Edges)
             {
                 var node = edge;
@@ -127,11 +157,14 @@ namespace ProgrammingQuestion4
                     {
                         //Console.WriteLine("Traversing node {0}", edge);
                         DfsRecursive(graph, eindex.Index);
+                        sccCount++;
                     }
                 }
             }
+            //Results[nodeIndex] = sccCount;
             t++;
-            Console.WriteLine("Finishing Time of v {1}= {0}", t, graph[nodeIndex].V);
+            FinishingTimes.Push(nodeIndex);
+            Console.WriteLine("Finishing Time of v {0} at index {1} = {2}", graph[nodeIndex].V, nodeIndex, t);
         }
 
         private static List<Node> ReadData(string filename)
@@ -154,6 +187,44 @@ namespace ProgrammingQuestion4
                     var index = inputData.Count;
                     inputData[index - 1].Edges.Add(edge);
                 }
+                else
+                {
+                    //New node so add vertex and edges
+                    var e = new List<int>();
+                    e.Add(edge);
+                    inputData.Add(new Node { V = currentNode, Edges = e, Visited = false, Index = inputData.Count });
+                }
+                v = currentNode;
+            }
+            return inputData;
+        }
+
+        private static List<Node> ReadDataReversed(string filename)
+        {
+            var txtData = File.ReadLines(filename).ToArray();
+            var inputData = new List<Node>();
+
+            var v = 0;
+
+            foreach (var s in txtData)
+            {
+                var x = s.Split(' ');
+
+                //Check if new node
+                var currentNode = Convert.ToInt32(x[1]);
+                var edge = Convert.ToInt32(x[0]);
+                var node = inputData.FirstOrDefault(n => n.V == currentNode);
+                if (node != null)
+                {
+                    //Add to current node
+                    inputData[node.Index].Edges.Add(edge);
+                }
+                //if (currentNode == v)
+                //{
+                //    //Add to current node
+                //    var index = inputData.Count;
+                //    inputData[index - 1].Edges.Add(edge);
+                //}
                 else
                 {
                     //New node so add vertex and edges
